842f4947ebc2d07bc1e8211696ded1a7be80455e
Lilgibbsy <owen@gibbsbunch.com>
2022-10-07 13:57:02 -0600

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -67,40 +67,82 @@ function level6Solution() {
 
 function level7Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = "----";
+  if ((row == 4) & (column == 7)) {
+    outputEl.innerHTML = "Water";
+  } else {
+    outputEl.innerHTML = "Sand";
+  }
 }
 
 function level8Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = "----";
+  if (row == 5 || column == 2) {
+    outputEl.innerHTML = "Tree";
+  } else {
+    outputEl.innerHTML = "Sand";
+  }
 }
 
 function level9Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = "----";
+  if (column <= 5 && row <= 5) {
+    outputEl.innerHTML = "Water";
+  } else {
+    outputEl.innerHTML = "Sand";
+  }
 }
 
 function level10Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = "----";
+  if (column <= 1) {
+    outputEl.innerHTML = "Hills";
+  } else if (column <= 3 && column >= 2) {
+    outputEl.innerHTML = "Tree";
+  } else if (column <= 7 && column >= 4) {
+    outputEl.innerHTML = "Sand";
+  } else {
+    outputEl.innerHTML = "Water";
+  }
 }
 
 function level11Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = "----";
+  if (column <= 5 && row <= 3) {
+    outputEl.innerHTML = "Water";
+  } else if (column <= 11 && row <= 3) {
+    outputEl.innerHTML = "Hills";
+  } else if (column <= 5 && row >= 4) {
+    outputEl.innerHTML = "Sand";
+  } else {
+    outputEl.innerHTML = "Tree";
+  }
 }
 
 function level12Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = "----";
+  if ((column <= 5 && row <= 4) || (column <= 11 && column >= 6 && row >= 5)) {
+    outputEl.innerHTML = "Water";
+  } else {
+    outputEl.innerHTML = "Sand";
+  }
 }
 
 function level13Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = "----";
+  if (row <= 5 && row >= 2 && column <= 9 && column >= 4) {
+    outputEl.innerHTML = "Sand";
+  } else {
+    outputEl.innerHTML = "Tree";
+  }
 }
 
 function level14Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = "----";
+  if (row <= 5 && row >= 3 && column <= 10 && column >= 7) {
+    outputEl.innerHTML = "Water";
+  } else if (row <= 5 && row >= 1 && column <= 4 && column >= 2) {
+    outputEl.innerHTML = "Tree";
+  } else {
+    outputEl.innerHTML = "Sand";
+  }
 }
